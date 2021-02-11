acebc8e19b071a1273af13c0b7f5302df8e75851
rplb1819-17-melanurhaliza <77601698+rplb1819-17-melanurhaliza@users.noreply.github.com>
2021-02-11 16:39:51 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Login</title>
+    <link rel ="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+  </head>
+
+  <body>
+    <section class="Form">
+        
+        <div class="container">
+        <div class="row content">
+            <div class="col-md-6 mb-3">
+                <img src="assets/img/l.jpg" class="img-fluid" alt="image">
+            </div>
+            <div class="col-md-6"><br>
+                <h3 class="signin-text mb-3" style="text-align: center;">Login</h3><br>
+
+                <form>
+                    
+                    <div class="form-group">
+                            <label for="username">Username</label>
+                            <input type="username" placeholder="Username" class="form-control">
+                    </div>
+                    <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" placeholder="Password" class="form-control">
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" name="checkbox" class="form-check-input" id="checkbox">
+                        <label class="form-check-label">Remember Me</label>
+                    </div>
+                    <button class="btn btn-success">Login</button>
+
+
+                </form>
+                
+            </div>
+        </div>
+    </div>
+
+    </section>
+    
+
+  </body>
+
+</html>
